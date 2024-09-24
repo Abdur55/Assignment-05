41a7c24b4a8f7460701986f739055d8f85b055b9
Abdur55 <skabdurrahim372@gmail.com>
2024-09-24 22:54:39 +0600

    my style commit

--- feature.js
+++ feature.js
@@ -4,41 +4,60 @@ const buttonTwo = document.getElementById('button-click2')
 
 
 
- Show history page 
+//  Show history page 
 
-// document.getElementById('button-click1').
-// addEventListener('click', function(){
+    document.getElementById('button-click1').
+    addEventListener('click', function(){
 
-//     const historiesPage = document.getElementById('histories-page').classList.add('hidden')
+  const historiesPage = document.getElementById('histories-page').classList.add('hidden')
     
-    // buttonOne.style.background = 'green'
-    // buttonTwo.style.background = '';
+     buttonOne.style.background = 'green'
+    buttonTwo.style.background = '';
 
 
-//     const donatingPage = document.getElementById('donation-page').classList.remove('hidden')
+    const donatingPage = document.getElementById('donation-page').classList.remove('hidden')
 
-// })
-
-
- Donation page 
-
-// document.getElementById('button-click2').
-// addEventListener('click', function(){
-
-//     const historiesPage = document.getElementById('histories-page').classList.remove('hidden')
-
-//     const donatingPage = document.getElementById('donation-page').classList.add('hidden')
-
-    //  buttonOne.style.background = '';
-    //  buttonTwo.style.background = 'green';
-
-// })
+    })
 
 
-// test history page
+//  Donation page 
 
-document.getElementById('button-click2')
-.addEventListener('click', function(){
-    
+document.getElementById('button-click2').
+addEventListener('click', function(){
+
+    const historiesPage = document.getElementById('histories-page').classList.remove('hidden')
+
+    const donatingPage = document.getElementById('donation-page').classList.add('hidden')
+
+     buttonOne.style.background = '';
+    buttonTwo.style.background = 'green';
+
 })
 
+
+// test history page 1
+
+// document.getElementById('button-click1')
+// .addEventListener('click', function(){
+
+
+// const historiPage = document.getElementById('histories-page').classList.add('hidden')
+
+
+// const donationPage = document.getElementById('donation-page').classList.remove('hidden')
+
+
+// })
+
+
+// test donation page 2
+
+// document.getElementById('button-click2')
+// .addEventListener('click', function(){
+
+//     const historiPage = document.getElementById('histories-page').classList.remove('hidden')
+
+
+// const donationPage = document.getElementById('donation-page').classList.add('hidden')
+
+// })

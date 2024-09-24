75655e1a8ecb3494ba41769f7ecc2dc70090540e
Abdur55 <skabdurrahim372@gmail.com>
2024-09-24 22:23:18 +0600

    my style commit

---
+++ feature.js
@@ -0,0 +1,44 @@
+
+const buttonOne = document.getElementById('button-click1')
+const buttonTwo = document.getElementById('button-click2')
+
+
+
+ Show history page 
+
+// document.getElementById('button-click1').
+// addEventListener('click', function(){
+
+//     const historiesPage = document.getElementById('histories-page').classList.add('hidden')
+    
+    // buttonOne.style.background = 'green'
+    // buttonTwo.style.background = '';
+
+
+//     const donatingPage = document.getElementById('donation-page').classList.remove('hidden')
+
+// })
+
+
+ Donation page 
+
+// document.getElementById('button-click2').
+// addEventListener('click', function(){
+
+//     const historiesPage = document.getElementById('histories-page').classList.remove('hidden')
+
+//     const donatingPage = document.getElementById('donation-page').classList.add('hidden')
+
+    //  buttonOne.style.background = '';
+    //  buttonTwo.style.background = 'green';
+
+// })
+
+
+// test history page
+
+document.getElementById('button-click2')
+.addEventListener('click', function(){
+    
+})
+

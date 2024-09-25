8dcbf0b5496debddb07b361e18e0e75934e6f987
Abdur55 <skabdurrahim372@gmail.com>
2024-09-25 09:05:27 +0600

    forth commit

--- index.js
+++ index.js
@@ -49,65 +49,116 @@ document.getElementById('btn-add-money')
     })
 
 
-// Two add money 2
+
+// Tow add money 2
 
 document.getElementById('btn2-add2-money2')
     .addEventListener('click', function (event) {
-        event.preventDefault();
+        event.preventDefault()
 
 
         const input2Add2 = document.getElementById('input2-add2-money2').value;
         const parse2Input2 = parseFloat(input2Add2)
+       
+        const addToCash = parseFloat(document.getElementById('cash-out').innerText);
+
+        if (isNaN(input2Add2)) {
+            alert('Please enter a number')
+            return;
+        }
 
 
-        const addMoney = document.getElementById('add-money').innerText;
-        const paresMoney = parseFloat(addMoney)
-
-        const total = parse2Input2 + paresMoney;
+        if (input2Add2 > 0 && input2Add2 <= addToCash) {
 
 
-        document.getElementById('add-money').innerText = total;
-
-        const addToCash = document.getElementById('cash-out').innerText;
-
-        const paresCashAdd = parseFloat(addToCash);
-
-        const total2 = paresCashAdd - parse2Input2;
+            modal_two.showModal()
 
 
-        document.getElementById('cash-out').innerText = total2
+
+            const addToMoney = parseFloat(document.getElementById('add-to-money').innerText);
+            const parsAddToMoney = parseFloat(addToMoney)
+
+            const total = parse2Input2 + parsAddToMoney;
+
+            document.getElementById('add-to-money').innerText = total;
+
+            const totalCashOut = addToCash - parse2Input2;
+            document.getElementById('cash-out').innerText = totalCashOut;
+
+
+        } else {
+            alert('Please enter a valid amount number')
+        }
+
     })
 
 
-// three add money 3
+// add money three ----------===
+document.getElementById('btn3-add3-money3')
+    .addEventListener('click', function (event) {
+        event.preventDefault()
+
+
+        const inputAdd3 = document.getElementById('input3-add3-money3').value;
+        const parseInput3 = parseFloat(inputAdd3);
+       
+        const addToCash = parseFloat(document.getElementById('cash-out').innerText);
+
+        if (isNaN(inputAdd3)) {
+            alert('Please enter a number')
+            return;
+        }
+
+
+        if (inputAdd3 > 0 && inputAdd3 <= addToCash) {
+
+
+            modal_three.showModal()
+
+
+
+            const addMoney3 = document.getElementById('add-money3').innerText;
+
+            const paresMoney3 = parseFloat(addMoney3);
+            console.log(paresMoney3);
+    
+            const total = parseInput3 + paresMoney3;
+            console.log(total)
+    
+            document.getElementById('add-money3').innerText = total;
+    
+            const addToCash = document.getElementById('cash-out').innerText;
+    
+            const paresCashAdd = parseFloat(addToCash);
+    
+            const total3 = paresCashAdd - parseInput3;
+            console.log(total3)
+    
+            document.getElementById('cash-out').innerText = total3;
+
+
+        } else {
+            alert('Please enter a valid amount number')
+        }
+
+    })
+
+
+
+
+
+
+
 
 document.getElementById('btn3-add3-money3')
     .addEventListener('click', function (event) {
         event.preventDefault();
 
-        const inputAdd3 = document.getElementById('input3-add3-money3').value;
-        const parseInput3 = parseFloat(inputAdd3);
-        console.log(parseInput3)
+
+    
 
 
-        const addMoney3 = document.getElementById('add-money3').innerText;
 
-        const paresMoney3 = parseFloat(addMoney3);
-        console.log(paresMoney3);
-
-        const total = parseInput3 + paresMoney3;
-        console.log(total)
-
-        document.getElementById('add-money3').innerText = total;
-
-        const addToCash = document.getElementById('cash-out').innerText;
-
-        const paresCashAdd = parseFloat(addToCash);
-
-        const total3 = paresCashAdd - parseInput3;
-        console.log(total3)
-
-        document.getElementById('cash-out').innerText = total3;
 
     })
 
